110020355085ea4f292bd6f94b28e1c287772048
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-01-12 10:00:39 -0500

    begin practice project, base components

---
+++ library/frontend/react-library/src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Navbar from "./layouts/NavbarAndFooter/Navbar";
+import ExploreTopBooks from "./layouts/Homepage/ExploreTopBooks";
+
+function App() {
+    return (
+        <>
+            <Navbar />
+            <ExploreTopBooks />
+        </>
+    );
+}
+
+export default App;
---
+++ library/frontend/react-library/src/layouts/Homepage/ExploreTopBooks.tsx
@@ -0,0 +1,22 @@
+const ExploreTopBooks = () => {
+    return (
+        <div className="p-5 mb-4 bg-dark header">
+            <div className="container-fluid py-5 text-white d-flex justify-content-center align-items-center">
+                <div>
+                    <h1 className="display-5 fw-bold">Find your next books</h1>
+                    <p className="col-md-10 fs-4">
+                        What wold you like to read next?
+                    </p>
+                    <a
+                        className="btn main-color btn-lg text-white"
+                        href="#"
+                        type="button"
+                    >
+                        Explore top books
+                    </a>
+                </div>
+            </div>
+        </div>
+    );
+};
+export default ExploreTopBooks;
