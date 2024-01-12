33f6d1dab686854a040172e72507a4f31d4f2e1b
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-01-11 21:22:46 -0500

    abstract

---
+++ ts-demo/demo106-abstract/ArrayDriver.ts
@@ -0,0 +1,20 @@
+import { Circle } from "./Circle";
+import { Rectangle } from "./Rectangle";
+import { Shape } from "./Shape";
+
+let myCircle = new Circle(5, 10, 20);
+
+let myRectangle = new Rectangle(7, 0, 3, 5);
+
+let theShapes: Shape[] = [];
+
+theShapes.push(myCircle);
+theShapes.push(myRectangle);
+
+for (let tempShape of theShapes) {
+    console.log(tempShape.getInfo());
+    console.log(tempShape.calculateArea());
+    console.log();
+}
+
+// Since Circle and Rectangle extends Shape they are of type Shape ?
