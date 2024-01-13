4b035587996dde35bfb544c9be5777faefa398f6
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-01-13 18:35:54 -0500

    search results page partial

--- library/frontend/react-library/src/App.tsx
+++ library/frontend/react-library/src/App.tsx
@@ -2,12 +2,14 @@ import "./App.css";
 import HomePage from "./layouts/Homepage/HomePage";
 import Footer from "./layouts/NavbarAndFooter/Footer";
 import Navbar from "./layouts/NavbarAndFooter/Navbar";
+import SearchBookPage from "./layouts/searchBookPage/SearchBookPage";
 
 function App() {
     return (
         <div>
             <Navbar />
-            <HomePage />
+            {/* <HomePage /> */}
+            <SearchBookPage />
             <Footer />
         </div>
     );
