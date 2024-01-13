0e9259c09c7fff8c7fe05c9ad8db513ba93f1c9b
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-01-12 23:05:17 -0500

    refactored

--- library/frontend/react-library/src/App.tsx
+++ library/frontend/react-library/src/App.tsx
@@ -1,22 +1,15 @@
-import React from "react";
 import "./App.css";
-import Navbar from "./layouts/NavbarAndFooter/Navbar";
-import ExploreTopBooks from "./layouts/Homepage/ExploreTopBooks";
-import Carousel from "./layouts/Homepage/Carousel";
-import Heros from "./layouts/Homepage/Heros";
-import LibraryServices from "./layouts/Homepage/LibraryServices";
+import HomePage from "./layouts/Homepage/HomePage";
 import Footer from "./layouts/NavbarAndFooter/Footer";
+import Navbar from "./layouts/NavbarAndFooter/Navbar";
 
 function App() {
     return (
-        <>
+        <div>
             <Navbar />
-            <ExploreTopBooks />
-            <Carousel />
-            <Heros />
-            <LibraryServices />
+            <HomePage />
             <Footer />
-        </>
+        </div>
     );
 }
 
---
+++ library/frontend/react-library/src/layouts/Homepage/HomePage.tsx
@@ -0,0 +1,16 @@
+import Carousel from "./components/Carousel";
+import ExploreTopBooks from "./components/ExploreTopBooks";
+import Heros from "./components/Heros";
+import LibraryServices from "./components/LibraryServices";
+
+const HomePage = () => {
+    return (
+        <>
+            <ExploreTopBooks />
+            <Carousel />
+            <Heros />
+            <LibraryServices />
+        </>
+    );
+};
+export default HomePage;
