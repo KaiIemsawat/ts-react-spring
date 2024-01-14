0a253caf0470c3f2b769d169e58d222cf2d35d2d
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-01-13 23:20:04 -0500

    Backend search by title

--- library/backend/springboot-library/src/main/java/com/demo/springbootlibrary/dao/BookRepo.java
+++ library/backend/springboot-library/src/main/java/com/demo/springbootlibrary/dao/BookRepo.java
@@ -1,8 +1,13 @@
 package com.demo.springbootlibrary.dao;
 
 import com.demo.springbootlibrary.enitiy.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public interface BookRepo extends JpaRepository<Book, Long> {
 
+//    Will create '"search": {"href": "http://localhost:8080/api/books/search"}' in JSON
+    Page<Book> findByTitleContaining(@RequestParam("title") String title, Pageable pageable);
 }
