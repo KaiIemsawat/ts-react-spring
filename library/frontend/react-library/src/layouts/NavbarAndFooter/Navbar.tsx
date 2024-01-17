73e8a551f7300eb9bbf2eb1b68663e63dcb28ca2
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-01-16 21:21:11 -0500

    Okta login, logout functioning

--- library/frontend/react-library/src/layouts/NavbarAndFooter/Navbar.tsx
+++ library/frontend/react-library/src/layouts/NavbarAndFooter/Navbar.tsx
@@ -1,6 +1,20 @@
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
+import { useOktaAuth } from "@okta/okta-react";
+import SpinerLoading from "../utils/SpinerLoading";
 
 const Navbar = () => {
+    const { oktaAuth, authState } = useOktaAuth();
+
+    if (!authState) {
+        return <SpinerLoading />;
+    }
+
+    const handleLogout = async () => {
+        oktaAuth.signOut();
+    };
+
+    console.log(authState);
+
     return (
         <nav className="navbar navbar-expand-lg navbar-dark main-color py-3">
             <div className="container-fluid">
@@ -33,15 +47,26 @@ const Navbar = () => {
                         </li>
                     </ul>
                     <ul className="navbar-nav ms-auto">
-                        <li className="nav-item m-1">
-                            <a
-                                href="#"
-                                className="btn btn-outline-light"
-                                type="button"
-                            >
-                                Sign in
-                            </a>
-                        </li>
+                        {!authState.isAuthenticated ? (
+                            <li className="nav-item m-1">
+                                <Link
+                                    to="/login"
+                                    className="btn btn-outline-light"
+                                    type="button"
+                                >
+                                    Sign in
+                                </Link>
+                            </li>
+                        ) : (
+                            <li className="nav-item m-1">
+                                <button
+                                    className="btn btn-outline-light"
+                                    onClick={handleLogout}
+                                >
+                                    Logout
+                                </button>
+                            </li>
+                        )}
                     </ul>
                 </div>
             </div>
