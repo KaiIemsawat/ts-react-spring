0a347fb7d1f64f1eaa1b725ed7f4388d9a8a7160
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-03-24 19:02:27 -0400

    mobile mobile fix

--- library-app-re/frontend/react-library/src/layouts/homepage/Carousel.tsx
+++ library-app-re/frontend/react-library/src/layouts/homepage/Carousel.tsx
@@ -1,3 +1,5 @@
+import { ReturnBook } from "./ReturnBook";
+
 export const Carousel = () => {
     return (
         <div className="container mt-5" style={{ height: 550 }}>
@@ -13,70 +15,25 @@ export const Carousel = () => {
                 <div className="carousel-inner">
                     <div className="carousel-item active">
                         <div className="row d-flex justify-content-center align-items-center">
-                            <div className="col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-3">
-                                <div className="text-center">
-                                    <img
-                                        src={require(`./../../Images/BooksImages/book-luv2code-1000.png`)}
-                                        alt="book"
-                                        width="151"
-                                        height="233"
-                                    />
-                                    <h6 className="mt-2">Book</h6>
-                                    <p>Love2Code</p>
-                                    <a
-                                        href="#"
-                                        className="btn main-color text-white"
-                                    >
-                                        Reserve
-                                    </a>
-                                </div>
-                            </div>
+                            <ReturnBook />
+                            <ReturnBook />
+                            <ReturnBook />
                         </div>
                     </div>
 
                     <div className="carousel-item">
                         <div className="row d-flex justify-content-center align-items-center">
-                            <div className="col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-3">
-                                <div className="text-center">
-                                    <img
-                                        src={require(`./../../Images/BooksImages/book-luv2code-1000.png`)}
-                                        alt="book"
-                                        width="151"
-                                        height="233"
-                                    />
-                                    <h6 className="mt-2">Book</h6>
-                                    <p>Love2Code</p>
-                                    <a
-                                        href="#"
-                                        className="btn main-color text-white"
-                                    >
-                                        Reserve
-                                    </a>
-                                </div>
-                            </div>
+                            <ReturnBook />
+                            <ReturnBook />
+                            <ReturnBook />
                         </div>
                     </div>
 
                     <div className="carousel-item">
                         <div className="row d-flex justify-content-center align-items-center">
-                            <div className="col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-3">
-                                <div className="text-center">
-                                    <img
-                                        src={require(`./../../Images/BooksImages/book-luv2code-1000.png`)}
-                                        alt="book"
-                                        width="151"
-                                        height="233"
-                                    />
-                                    <h6 className="mt-2">Book</h6>
-                                    <p>Love2Code</p>
-                                    <a
-                                        href="#"
-                                        className="btn main-color text-white"
-                                    >
-                                        Reserve
-                                    </a>
-                                </div>
-                            </div>
+                            <ReturnBook />
+                            <ReturnBook />
+                            <ReturnBook />
                         </div>
                     </div>
                 </div>
@@ -108,20 +65,8 @@ export const Carousel = () => {
 
             {/* MOBILE */}
             <div className="d-lg-none mt-3">
-                <div className="row d-flex justfy-content-center align-items-center">
-                    <div className="text-center">
-                        <img
-                            src={require(`./../../Images/BooksImages/book-luv2code-1000.png`)}
-                            alt="Book"
-                            width="151"
-                            height="233"
-                        />
-                        <h6 className="mt-2">
-                            <b>Book</b>
-                        </h6>
-                        <p>Love2Code</p>
-                        <a href="#" className="btn main-color text-white"></a>
-                    </div>
+                <div className="row d-flex justify-content-center align-items-center">
+                    <ReturnBook />
                 </div>
             </div>
             <div className="homepage-carousel-title mt-3">
