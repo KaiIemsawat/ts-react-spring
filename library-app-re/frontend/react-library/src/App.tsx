c79248c14121318950d05101e5bb658410d41ccb
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-03-25 09:37:29 -0400

    hero

--- library-app-re/frontend/react-library/src/App.tsx
+++ library-app-re/frontend/react-library/src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import { Navbar } from "./layouts/navbarAndFooter/Navbar";
 import { ExploreTopBooks } from "./layouts/homepage/ExploreTopBooks";
 import { Carousel } from "./layouts/homepage/Carousel";
+import { Hero } from "./layouts/homepage/Hero";
 
 function App() {
     return (
@@ -10,6 +11,7 @@ function App() {
             <Navbar />
             <ExploreTopBooks />
             <Carousel />
+            <Hero />
         </div>
     );
 }
