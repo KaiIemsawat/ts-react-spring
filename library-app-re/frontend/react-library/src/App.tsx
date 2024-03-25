c8c8ca409d7caf7d8b23fefa7cd2eae8cd41b89c
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-03-25 09:44:31 -0400

    services

--- library-app-re/frontend/react-library/src/App.tsx
+++ library-app-re/frontend/react-library/src/App.tsx
@@ -4,6 +4,7 @@ import { Navbar } from "./layouts/navbarAndFooter/Navbar";
 import { ExploreTopBooks } from "./layouts/homepage/ExploreTopBooks";
 import { Carousel } from "./layouts/homepage/Carousel";
 import { Hero } from "./layouts/homepage/Hero";
+import { LibraryServices } from "./layouts/homepage/LibraryServices";
 
 function App() {
     return (
@@ -12,6 +13,7 @@ function App() {
             <ExploreTopBooks />
             <Carousel />
             <Hero />
+            <LibraryServices />
         </div>
     );
 }
